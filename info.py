c5c82183391256e5bb3a42975ee45b9e2bd1e167
kashif Ali Sayyed <151992517+Sayyedkashif@users.noreply.github.com>
2024-08-31 17:57:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -123,7 +123,7 @@ if len(BIN_CHANNEL) == 0:
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "https://associated-remy-nchautofilter-91ed9bfd.koyeb.app/") #if heroku then paste the app link here ex: https://heroku......./
+URL = environ.get("URL", "bizarre-blanch-nchautofilter-4a21e2db.koyeb.app/") #if heroku then paste the app link here ex: https://heroku......./
 if len(URL) == 0:
     print('Error - URL is missing, exiting now')
     exit()
